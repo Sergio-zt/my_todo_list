daa6aee3a0726bc0ade384c5d490cd33a844265d
Sergio-zt <fop.gso@gmail.com>
2026-07-23 16:08:20 -0400

    Configuring base template and views classes

---
+++ todolist/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from todolist.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    toggle_task_status,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
+
+
+urlpatterns = [
+    path('', TaskListView.as_view(), name='task-list'),
+    path('task/create/', TaskCreateView.as_view(), name='task-create'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+    path('task/<int:pk>/toggle/', toggle_task_status, name='task-toggle'),
+    path('tags/', TagListView.as_view(), name='tag-list'),
+    path('tags/', TagListView.as_view(), name='tag-list'),
+    path('tags/create/', TagCreateView.as_view(), name='tag-create'),
+    path('tags/<int:pk>/update/', TagUpdateView.as_view(), name='tag-update'),
+    path('tags/<int:pk>/delete/', TagDeleteView.as_view(), name='tag-delete'),
+]
+
+app_name = "todolist"
